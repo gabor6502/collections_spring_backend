02f2689903e171894b0b3e46daee65b4aa95599c
gabor6502 <elan.gabor@gmail.com>
2025-08-12 18:20:48 -0500

    get mapping added to controller with basic exception handling

--- src/main/java/com/api/collections/controllers/ItemController.java
+++ src/main/java/com/api/collections/controllers/ItemController.java
@@ -1,9 +1,32 @@
 package com.api.collections.controllers;
 
+import com.api.collections.controllers.exceptions.NoIdException;
+import com.api.collections.serializables.ItemSerializable;
+import com.api.collections.services.exceptions.notFound.ItemNotFoundException;
+import com.api.collections.services.item.ItemService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("item")
 public class ItemController 
 {
+    @Autowired
+    protected ItemService service;
+    
+    @GetMapping("/{id}")
+    public ItemSerializable getItem(@PathVariable Long id) throws ItemNotFoundException, NoIdException
+    {
+        if (id == null)
+        {
+            throw new NoIdException();
+        }
+            
+        return service.getItem(id);
+    }
     
 }
