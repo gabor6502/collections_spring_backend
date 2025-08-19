05ad293de8cadc242b049378ab6cd07f30b84d45
gabor6502 <elan.gabor@gmail.com>
2025-08-19 10:22:25 -0500

    error message touch up

--- src/main/java/com/api/collections/services/exceptions/NoRelevantQueryException.java
+++ src/main/java/com/api/collections/services/exceptions/NoRelevantQueryException.java
@@ -4,6 +4,6 @@ public class NoRelevantQueryException extends Exception
 {
     public NoRelevantQueryException(Class c)
     {
-        super("A query relevant to the management of this "+c.getName()+" has not been created.");
+        super("A query relevant to the management of the "+c.getName()+" foreign entity has not been created.");
     }
 }
